54f9fe8ab0d0a9230b3507e744bb2e64b91f3945
Natchapol <natchapol.saiwoothi@hotmail.com>
2025-04-04 09:35:05 +0700

    updateFunctionSearchbar

--- src/components/navbar/Searchbar.jsx
+++ src/components/navbar/Searchbar.jsx
@@ -1,11 +1,31 @@
-import { Input } from "../ui/input"
+import { useSearchParams } from "react-router";
+import { Input } from "../ui/input";
+import { useDebounce, useDebouncedCallback } from "use-debounce";
+
 const Searchbar = () => {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const updateSearch = useDebouncedCallback((value) => {
+    console.log(value);
+    const params = new URLSearchParams(searchParams)
+    if(value){
+      params.set('search',value)
+    }else{
+      params.delete('search')
+    }
+    setSearchParams(params)
+  }, 500);
+
+  const hdlSearch = (e) => {
+    updateSearch(e.target.value);
+  };
+
   return (
     <Input
-    type="text"
-    placeholder="Search..."
-    className="max-w-sm"
+      onChange={hdlSearch}
+      type="text"
+      placeholder="Search..."
+      className="max-w-sm"
     />
-  )
-}
-export default Searchbar
+  );
+};
+export default Searchbar;
